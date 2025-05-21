1d4a7bc5dd252f285c4a12100b483ef57c6ae97e
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-22 05:46:09 +0700

    create view home view

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+    <HeroSection />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Navbar from '@/components/NavbarComponent.vue'
+import HeroSection from '@/components/HeroSection.vue'
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    Navbar,
+    HeroSection,
+  },
+})
+</script>
