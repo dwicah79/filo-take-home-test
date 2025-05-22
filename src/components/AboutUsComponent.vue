159e2eacdfb507e4c05c29dbb5bb81a09e9c70fe
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-22 07:20:24 +0700

    hotfix: add special menu in aboutus component

--- src/components/AboutUsComponent.vue
+++ src/components/AboutUsComponent.vue
@@ -38,11 +38,86 @@
       </div>
     </div>
   </section>
+
+  <section class="mt-5 md:mt-30 px-10 bg-white">
+    <div class="container mx-auto px-6">
+      <h1 class="font-bold text-2xl md:text-3xl mb-12">
+        Special menu <span class="border-b-4 border-orange-500">for you</span>
+      </h1>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10">
+        <div
+          v-for="(item, index) in getspecialmenu"
+          :key="item.id"
+          class="bg-white/40 md:shadow-2xl rounded-2xl p-2 shadow transition-all duration-300"
+        >
+          <div class="rounded-2xl p-5 bg-white">
+            <div
+              class="h-60 w-full mb-6 flex items-center justify-center rounded-lg overflow-hidden"
+            >
+              <div class="relative w-full">
+                <img :src="getImageUrl(item.image)" :alt="item.name" class="h-full w-full" />
+                <div class="absolute rounded-full p-1 bg-white/50 top-10 left-2">
+                  <div class="inline-flex bg-white items-center rounded-full gap-1 px-2 py-0.5">
+                    <span class="font-bold">{{ item.rating }}</span>
+                    <span><i class="fa-solid fa-star text-yellow-300"></i></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="inline-flex justify-between w-full items-center mx-auto">
+              <h3 class="text-2xl font-bold mb-4 text-center">
+                {{ item.name }}
+              </h3>
+              <span class="text-2xl font-bold">{{ item.price }}</span>
+            </div>
+            <div class="flex w-full gap-3 mb-6">
+              <span class="font-semibold text-gray-400">{{ item.description }}</span>
+              <div class="flex w-full justify-end p-2">
+                <button class="btn-secondary group-hover:bg-orange-300 cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+import { getSpecialItems, SpecialItem } from '@/mock/api'
 export default defineComponent({
   name: 'AboutUsComponent',
+
+  setup() {
+    const getspecialmenu = ref<SpecialItem[]>([])
+    const getImageUrl = (imageName: string) => {
+      return `/images/${imageName}`
+    }
+    onMounted(() => {
+      setTimeout(() => {
+        getspecialmenu.value = getSpecialItems()
+      }, 300)
+    })
+    return {
+      getspecialmenu,
+      getImageUrl,
+    }
+  },
 })
 </script>
